c29eef5b438ba65f859e9d050fda128fd5b0e8f6
Trinie J <jxsn00ks@gmail.com>
2020-06-16 15:56:02 -0400

    corrected class

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ newButton.append('Remove')
 bodyElement.append(newButton)
 
 let newMain = document.createElement ('Main')
-newMain.className = 'classImg'
 bodyElement.append(newMain)
 
 let classImg = document.createElement('img')
 classImg.src = 'http://www.xclusivetouch.co.uk/wp-content/uploads/2014/06/the-it-crowd-funny-computer-gif.gif'
+classImg.className = 'img'
 newMain.append(classImg)
 bodyElement.append(newMain)
 
-let classAnchor = document.createElement('a')
+let classAnchor = document.createElement('link')
 classAnchor.href = 'https://www.imdb.com/title/tt0487831/'
 classAnchor.append("See more IT Crowd")
+classAnchor.className = 'link'
 newMain.append(classAnchor)
 bodyElement.append(newMain)
-
-
-
 
 
 newButton.addEventListener("click", function(){
